2e9a47d23bd75f60b7d58c981e2fac29382ebd1b
mingzitaichangtinghao <1183538946@qq.com>
2019-04-03 11:11:13 +0800

    你好

---
+++ src/com/senchen365/chen/Hello.java
@@ -0,0 +1,17 @@
+package com.senchen365.chen;
+
+/**
+ * 类名:Hello<br>
+ * 功能:<br>
+ * 作者:李帅<br>
+ * 日期:2019/4/3<br>
+ * 版本:1.0.0<br>
+ */
+public class Hello {
+    /**
+     * 方法主入口
+     */
+    public static void main(String[]arge){
+        System.out.println("HELLO");
+    }
+}
